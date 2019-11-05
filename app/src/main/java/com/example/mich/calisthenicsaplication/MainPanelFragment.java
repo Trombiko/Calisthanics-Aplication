003f73a37973a5b4835171fcf1521fb7b4e158e4
Trombiko <michal.trombik@gmail.com>
2019-11-05 17:50:47 +0100

    Przeniesienie zbyt dużych map bitowych do folderu drawable-xxxhdpi. Dodanie pliku main_fragment.xml, uzupełnienie go od strony wizualnej. Podpięcie go pod klasę MainPanelFragment.

--- app/src/main/java/com/example/mich/calisthenicsaplication/MainPanelFragment.java
+++ app/src/main/java/com/example/mich/calisthenicsaplication/MainPanelFragment.java
@@ -1,10 +1,27 @@
 package com.example.mich.calisthenicsaplication;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class MainPanelFragment extends Fragment implements View.OnClickListener
 {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+
+
+
+
+
+
+        return rootView;
+    }
 
 
 
